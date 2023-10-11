5a2a88398b9c98f0988dbbfc14ea3660781d5254
Tadeáš Fenik <fenikt22@spstrutnov.cz>
2023-10-11 13:56:08 +0200

    začátek piškvorek
    zkouším něco dělat podle stránky a zároveň se to snažit pochopit a naučit

---
+++ piskvorky/piskvorky/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace piskvorky
+{
+    internal class Program
+    {
+        static char[] pole = { '0' , '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        static int hrac = 0;
+        static int kontrola;
+        static int flag = 0;
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hráč 1:X    Bot:O");
+            Board();
+            kontrola = int.Parse(Console.ReadLine());
+            if (pole[kontrola] != "X" && pole[kontrola] != "O") 
+            {
+                pole[kontrola] = "O";
+                hrac++;
+            }
+            else
+            {
+                pole[kontrola] = "X";
+                hrac++;
+            }
+            else
+            {
+                Console.WriteLine("A je je poličko {0} je zabrané {1}");
+                Console.WriteLine("zkus to znovu");
+            }
+            flag = CheckWin();
+            }
+        while (flag != 1 && flag != -1);
+        console.clear();
+        Board();
+
+    }
+}
---
+++ piškvorky/piškvorky/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace piškvorky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("piškvorrkyyyyy!!!");
+
+            List<string> pole;
+            pole = new List<string>();
+
+            pole.Add(" ");
+            pole.Add("1");
+            pole.Add("2");
+            pole.Add("3");
+            pole.Add("4");
+            pole.Add("5");
+            pole.Add("6");
+            pole.Add("7");
+            pole.Add("8");
+            pole.Add("9");
+
+            Console.WriteLine(pole[2]);
+            pole[2] = "AAA";
+            for (int i = 0; i < pole.Count; i++)
+            {
+                Console.Write(pole[i] + " ");
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(" ");
+                }
+
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("kam chceš položit X?");
+            Console.Write("Tvá odpověď: ");
+            string hrac = Console.ReadLine();
+
+            if (pole[1] == hrac || pole[2] == hrac || pole[1] == hrac || pole[1] == hrac || pole[1] == hrac || pole[1] == hrac)
+            {
+                pole[0] = "X";
+            }
+
+
+        }
+    }
+}
