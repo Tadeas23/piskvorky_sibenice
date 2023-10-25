b61e457ecab70813986d7e2cebbc9be98d3f1cd2
Tadeáš Fenik <fenikt22@spstrutnov.cz>
2023-10-25 13:58:08 +0200

    novy zacatek s piskvorkama

---
+++ piskovrky_2/piskovrky_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace piskvorky_2
+{
+    internal class Program
+    {
+        static string[] pole = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        static void Main()
+        {
+            Console.WriteLine("      |      |      ");
+            Console.WriteLine("   {0}  |   {1}  |   {2}   ", pole[1], pole[2], pole[3]);
+            Console.WriteLine("      |      |      ");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("      |      |      ");
+            Console.WriteLine("   {0}  |   {1}  |   {2}   ", pole[4], pole[5], pole[6]);
+            Console.WriteLine("      |      |      ");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("      |      |      ");
+            Console.WriteLine("   {0}  |   {1}  |   {2}   ", pole[7], pole[8], pole[9]);
+            Console.WriteLine("      |      |      ");
+
+            Random random = new Random();
+            int nahodneCislo = random.Next(1, 10);
+
+            string bot = nahodneCislo.ToString();
+
+            
+
+
+
+        }        
+    }
+}
